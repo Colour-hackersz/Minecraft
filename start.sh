3431002e1c3d521ff1ae30f46aa8a5a3ab8af767
Colour-hackersz <amatlib62@gmail.com>
2026-03-29 01:47:40 +0530

    Refactor start.sh for improved server setup
    Updated start.sh to streamline server setup and installation.

--- start.sh
+++ start.sh
@@ -2,21 +2,30 @@
 
 echo "Starting Minecraft server..."
 
+apt update
+apt install -y openjdk-17-jdk curl wget unzip
+
 accept eula
 
 echo "eula=true" > eula.txt
 
-install dependencies
+download server jar
 
-apt update
-apt install -y openjdk-17-jdk curl
+if [ ! -f server.jar ]; then
+wget https://api.papermc.io/v2/projects/paper/versions/1.21.11/builds/127/downloads/paper-1.21.11-127.jar -O server.jar
+fi
 
-install playit
+download world (REPLACE WORLD_FILE_ID)
+
+if [ ! -d world ]; then
+echo "Downloading world..."
+wget "https://drive.google.com/uc?export=download&id=WORLD_FILE_ID" -O world.zip
+unzip -o world.zip
+fi
+
+install playit tunnel
 
 curl -SsL https://playit.cloud/setup.sh | bash
-
-start tunnel
-
 ./playit &
 
 run server (auto restart)
